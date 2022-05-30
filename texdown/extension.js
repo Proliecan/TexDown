a5c817a8facfd2604f49bbb16c7d9ead1dfdf9a7
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-05-30 13:09:10 +0200

    Parse bold text

--- texdown/extension.js
+++ texdown/extension.js
@@ -1,36 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "texdown" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('texdown.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('texdown.convert', async function() {
+		
+		// read text from open file
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+		let text = editor.document.getText();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TexDown!');
+		text = parseBold(text);
+
+		// write text to new file with .tex extension
+		let newFile = vscode.Uri.parse('untitled:' + editor.document.fileName + '.tex');
+		let newEditor = vscode.window.showTextDocument(newFile);
+		(await newEditor).edit(edit => {
+			edit.insert(new vscode.Position(0, 0), text);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
 	activate,
 	deactivate
+}
+
+// utility functions
+let parseBold = (text) => {
+	// get regex for **<sometext>**
+	let bold = /\*\*(.*?)\*\*/g;
+	// match regex
+	let matches = text.match(bold);
+	// replace regex with \\textbf{<sometext>}
+	for (let i = 0; i < matches.length; i++) {
+		let match = matches[i];
+		let replacement = '\\textbf{' + match.substring(2, match.length - 2) + '}';
+		text = text.replace(match, replacement);
+	}
+	return text;
 }
